2c6f02b831375124ecef4a409f89e163184e6b2a
ritwikjamuar <ritwikjamuar@gmail.com>
2021-09-07 22:46:02 +0530

    Added various Kotlin files that manages the dependencies around the project.

---
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -0,0 +1,33 @@
+/**
+ * Encapsulates the Common Build Configuration values.
+ *
+ * @author Ritwik Jamuar
+ */
+object Configuration {
+
+    /**
+     * Compile SDK Version.
+     */
+    const val compileSDKVersion = 30
+
+    /**
+     * Android Build Tools Version.
+     */
+    const val buildToolsVersion = "30.0.3"
+
+    /**
+     * Minimum SDK Version.
+     */
+    const val minimumSDKVersion = 21
+
+    /**
+     * Target SDK Version.
+     */
+    const val targetSDKVersion = 30
+
+    /**
+     * Java Version for compilation.
+     */
+    const val javaVersion = "1.8"
+
+}

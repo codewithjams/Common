6a10af0dd0847dc4d22128828c9998030c76334c
ritwikjamuar <ritwikjamuar@gmail.com>
2021-09-07 22:50:19 +0530

    Added custom Gradle Plugins to reduce Boiler-Plate Code for Gradle Scripts.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,6 +4,35 @@ plugins {
     `kotlin-dsl`
 }
 
+gradlePlugin {
+
+    plugins {
+
+        register("library-module-plugin") {
+            id = "library-module-plugin"
+            implementationClass = "LibraryModulePlugin"
+        }
+
+        register("application-module-plugin") {
+            id = "application-module-plugin"
+            implementationClass = "ApplicationModulePlugin"
+        }
+
+    }
+
+}
+
 repositories {
+    google()
     mavenCentral()
+}
+
+dependencies {
+
+    compileOnly(gradleApi())
+
+    implementation("com.android.tools.build:gradle:7.0.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    implementation(kotlin("android-extensions"))
+
 }

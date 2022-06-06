0f2a624d06a418af046fe87f257fee848fa6a963
ritwikjamuar <g!t1-1u6>
2022-06-07 01:17:15 +0530

    Refactored the module `common_mvvm` to make the code as agnostic of UI as possible, and removing hard-implementation required with optional protected methods which consumer can opt whether to override or not, instead of forcing them to implement all the abstract methods which may not be used in most use-cases. Also, the module `common_mvvm` now depends on `common_lifecycle` so as to enable DI out of box.

--- common_mvvm/build.gradle.kts
+++ common_mvvm/build.gradle.kts
@@ -5,19 +5,7 @@ plugins {
 
 dependencies {
 
-    api(project(":common_data_ui"))
-
-    api(Dependencies.JetPack.Lifecycle.viewModel)
-    api(Dependencies.JetPack.Lifecycle.liveData)
-    api(Dependencies.JetPack.Lifecycle.runTime)
-
-    implementation(Dependencies.Kotlin.standardLibrary)
-    implementation(Dependencies.Google.Dagger.standardLibrary)
-    implementation(Dependencies.Google.Dagger.android)
-
-    kapt(Dependencies.Google.Dagger.androidProcessor)
-    kapt(Dependencies.Google.Dagger.compiler)
-    kapt(Dependencies.JetPack.Lifecycle.compiler)
+    api(project(":common_lifecycle"))
 
     testImplementation(Dependencies.JUnit.standardLibrary)
 
--- common_mvvm/src/main/java/com/droidboi/common/mvvm/model/BaseModel.kt
+++ common_mvvm/src/main/java/com/droidboi/common/mvvm/model/BaseModel.kt
@@ -1,24 +1,8 @@
 package com.droidboi.common.mvvm.model
 
-import com.droidboi.common.data.ui.PopUpData
-import com.droidboi.common.data.ui.ProgressData
-
-import com.droidboi.common.mvvm.data.ErrorData
-
-import com.droidboi.common.mvvm.utility.ACTION_NONE
-
 /**
- * Abstract Model to contain common data related to UI.
+ * Marks an implementation as a class holding the data associated to UI.
  *
- * @param action [Integer] denoting the identifier of the Action.
- * @param progressData Instance of [ProgressData].
- * @param errorData Instance of [ErrorData].
- * @param popUpData Instance of [PopUpData].
  * @author Ritwik Jamuar
  */
-abstract class BaseModel(
-    var action: Int = ACTION_NONE,
-    var progressData: ProgressData = ProgressData(),
-    var errorData: ErrorData = ErrorData(),
-    var popUpData: PopUpData = PopUpData()
-)
+interface BaseModel

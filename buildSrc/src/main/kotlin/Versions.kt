9736d719c0b0dc65a82c930f5f92d5f9783bb8f3
ritwikjamuar <g!t1-1u6>
2022-06-06 17:32:29 +0530

    Removed the unused dependencies and versions from Dependencies.kt and Versions.kt respectively.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -45,7 +45,7 @@ object Versions {
         }
 
         object DataBinding {
-            const val standardLibrary = gradle
+            const val standardLibrary = Gradle.version
         }
 
         object DataStore {
@@ -152,38 +152,5 @@ object Versions {
         }
 
     }
-
-    const val gradle = "7.2.1"
-
-    const val kotlin = "1.5.30"
-    const val kotlinCoRoutines = "1.4.3"
-
-    const val androidXAppCompat = "1.3.1"
-    const val androidXLegacyV4 = "1.0.0"
-    const val annotation = "1.2.0"
-    const val recyclerView = "1.2.1"
-    const val googleMaterialDesign = "1.4.0"
-    const val cardView = "1.0.0"
-    const val constraintLayout = "2.1.0"
-    const val facebookShimmer = "0.5.0"
-    const val superChargeShimmer = "2.1.0"
-    const val dagger = "2.42"
-    const val coreKTX = "1.6.0"
-    const val lifecycleViewModel = "2.3.1"
-    const val lifecycleCommon = "2.4.0-alpha03"
-    const val lifecycleExtension = "2.2.0"
-    const val retrofit = "2.9.0"
-    const val picasso = "2.71828"
-    const val picassoDownloader = "1.1.0"
-    const val loggingInterceptor = "4.9.0"
-    const val dataStorePreference = "1.0.0"
-    const val jetpackNavigation = "2.3.5"
-    const val gson = "2.8.6"
-    const val leakCanary = "2.7"
-
-    const val jUnitVersion = "4.13.2"
-
-    const val androidXJUnitVersion = "1.1.3"
-    const val androidXEspresso = "3.4.0"
 
 }
